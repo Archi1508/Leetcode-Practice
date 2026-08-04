3f6734d89c6fe8cc42f81818e9e42b6315aaca04
Archi1508 <143097029+Archi1508@users.noreply.github.com>
2026-08-04 09:19:44 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.52%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int count0 = 0, count1 = 0, count2 = 0;
+        int n = nums.size();
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
 
-        // Count frequencies
-        for (int num : nums) {
-            if (num == 0)
-                count0++;
-            else if (num == 1)
-                count1++;
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
             else
-                count2++;
+            {
+                swap(nums[mid],nums[high]);
+                high--;
+            }
         }
-
-        // Overwrite the array
-        int index = 0;
-
-        while (count0--)
-            nums[index++] = 0;
-
-        while (count1--)
-            nums[index++] = 1;
-
-        while (count2--)
-            nums[index++] = 2;
     }
 };
