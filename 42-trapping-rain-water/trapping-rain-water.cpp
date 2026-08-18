03f7a13c0c00b02a4d566166648aa0a0ebd54783
Archi1508 <143097029+Archi1508@users.noreply.github.com>
2026-08-19 01:32:10 +0530

    Time: 2 ms (18.71%) | Memory: 26 MB (77.76%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,30 +1,62 @@
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int n = height.size();
+//         vector<int>leftMax(n,0);
+//         vector<int>rightMax(n,0);
+//         leftMax[0] = height[0];
+//         rightMax[n-1] =  height[n-1];
+
+//         int ans = 0;
+
+//         for(int i=1;i<n;i++)
+//         {
+//             leftMax[i] = max(leftMax[i-1],height[i]);
+//         }
+
+//         for(int i=n-2;i>=0;i--)
+//         {
+//             rightMax[i] = max(rightMax[i+1],height[i]);
+//         }
+
+//         for(int i=0;i<n;i++)
+//         {
+//             ans+= min(leftMax[i],rightMax[i])-height[i];
+//         }
+//         return ans;
+
+
+//     }
+// };
+
+
+//TWO POINTER -  AS WE ARE USING 2 EXRA SPACES TO STORE MAX EVERYTIME RATHER WE CAN USE TWO POINTERS AS THERE IS NO NEED IN FUTURE OF THESE VALUES.
+
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int>leftMax(n,0);
-        vector<int>rightMax(n,0);
-        leftMax[0] = height[0];
-        rightMax[n-1] =  height[n-1];
+       int n = height.size();
+       int l = 0;
+       int r = n-1;
+       int ans = 0;
+       int leftMax = 0;
+       int rightMax = 0;
+       while(l<r)
+       {
+        leftMax = max(leftMax,height[l]);
+        rightMax = max(rightMax,height[r]);
 
-        int ans = 0;
-
-        for(int i=1;i<n;i++)
+        if(leftMax < rightMax)
         {
-            leftMax[i] = max(leftMax[i-1],height[i]);
+            ans += leftMax - height[l];
+            l++;
         }
-
-        for(int i=n-2;i>=0;i--)
+        else
         {
-            rightMax[i] = max(rightMax[i+1],height[i]);
+            ans += rightMax - height[r];
+            r--;
         }
-
-        for(int i=0;i<n;i++)
-        {
-            ans+= min(leftMax[i],rightMax[i])-height[i];
-        }
-        return ans;
-
-
+       }
+       return ans;
     }
 };
